cb01ff9ee7390a8b24bf2e3df03419cf556e9b6a
Maksym Salov <maksimsalov8@gmail.com>
2022-07-20 17:23:25 +0300

    add registration page

--- src/app/header/login/registration/registration.component.ts
+++ src/app/header/login/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RegistrationService } from 'src/app/shared/services/registration.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm! : FormGroup;
+
+  constructor(private register: RegistrationService) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      'name' : new FormControl('',[Validators.required]),
+      'email': new FormControl('',[Validators.required, Validators.email]),
+      'password': new FormControl('',[Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)])
+    })
+  }
+
+  get formControls(){
+    return this.registerForm.controls
+  }
+
+
+  submitRegister(){
+    this.register.registerUser(this.registerForm.value)
+    console.log(this.registerForm.value)
   }
 
 }
--- src/app/shared/services/registration.service.ts
+++ src/app/shared/services/registration.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map,mergeMap } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
+  dbUsers = 'http://localhost:3001/users';
+  users = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  checkPasswords(){
+    
+  }
+  registerUser(user: User){
+    this.http.get(this.dbUsers).pipe(
+      map(el => console.log(Array.isArray(el)))
+    ).subscribe()
+    
+    // this.http.post(this.dbUsers,user).subscribe()
+  }
 }
