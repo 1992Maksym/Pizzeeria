c5912826f1e4b309f5f0c70b27b66e0593394c4a
Maksym Salov <maksimsalov8@gmail.com>
2023-02-10 17:04:12 +0200

    fix(app): add MatDialog and delete login and register components from app.routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from "@angular/core";
-import {ExtraOptions, RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {MainPageComponent} from "./main-page/main-page.component";
 import {AboutUsComponent} from "./about-us/about-us.component";
 import {PromotionsComponent} from "./promotions/promotions.component";
-import {LoginComponent} from "./login/login.component";
 import {NotFoundComponent} from "./shared/components/not-found/not-found.component";
-import {RegistrationComponent} from "./registration/registration.component";
 import {UserComponent} from "./login/user/user.component";
 import {AdminComponent} from "./login/admin/admin.component";
 import { UserAuthGuard } from "./shared/guards/user-auth.guard";
@@ -18,8 +16,6 @@ const routes: Routes = [
   { path: '', component: MainPageComponent, resolve:{pizzaList: PizzaListResolver} },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'promotions', component: PromotionsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent },
   { path: 'admin', component: AdminComponent, canActivate: [UserAuthGuard] },
   { path: 'user/:id', component: UserComponent, canActivate: [UserAuthGuard] },
   { path: 'pizza/:id', component: PizzaViewComponent, resolve:{pizza: PizzaViewResolver} },
